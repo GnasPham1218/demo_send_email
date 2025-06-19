b8b08b961979c47f8f73abebbfd5119a7fd91e0b
Gnas Pham <gnasai1218@gmail.com>
2025-06-19 11:37:20 +0700

    config vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, 
+    port: 3000,
   },
+  build: {
+    outDir: 'dist', // this is default for Vite
+  },
+  base: './', // important for correct asset loading on Render
 })
+
